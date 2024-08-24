1c023d0ae778b714a19e5fd621502a84ddba3a2e
Gal Ben Ami <102553622+benami171@users.noreply.github.com>
2024-08-24 12:59:23 +0300

    some progress..

--- QUIC.py
+++ QUIC.py
@@ -1,5 +1,5 @@
 import random
-import socket
+from socket import socket ,AF_INET, SOCK_DGRAM
 import math
 import struct
 import time
@@ -9,8 +9,8 @@ from random import randint
 from typing import Dict, List, Tuple
 from dataclasses import dataclass
 
-from numpy.distutils.conv_template import header
 
+OVERALL_DATA = 0 # The position in the stats dictionary for overall data that was sent/received
 
 class FLAGS(IntEnum):
     SYN = 1
@@ -26,30 +26,31 @@ class QUIC_CONNECTION:
 
     def __init__(self):
         # Initialize attributes for the connection state
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock = socket(AF_INET,SOCK_DGRAM)
         self._is_closed = False
 
         # Variables to store host and port for reuse
         self.host_address = None
-        self.port_number = None
+        self.port = None
 
         # Initialize stream-related attributes
         self.stream_ID = 0  # each stream has a unique ID
 
         # Dictionaries to store incoming and outgoing streams
+        # Dictionaries to store stream statistics, and connection statistics for further analysis
         self.in_streams: Dict[int, bytes] = {}
         self.out_streams: Dict[int, bytes] = {}
+        self.streams_stats: Dict[int, Stats] = {}
+        self.connection_stats: Dict[int, Stats] = {}
 
-        # Dictionary to store stream statistics for further analysis
-        self.stream_stats: Dict[int, Stream_Statistics] = {}
 
     def listen_incoming_connections(self, host: str, port: int):
         """Listen for incoming connections."""
         print(f"Listening for incoming connections on {host}:{port}")
         self.host_address = host
-        self.port_number = port
+        self.port = port
         # bind the socket to the host and port
-        self.sock.bind((self.host_address, self.port_number))
+        self.sock.bind((self.host_address, self.port))
         received_data, address = self.sock.recvfrom(QUIC_PACKET.Max_size)
 
         received_packet = QUIC_PACKET.deserialize_data(received_data)[0]
@@ -62,12 +63,12 @@ class QUIC_CONNECTION:
             raise Exception("Unexpected packet received")
 
 
-    def connect(self, host: str, port: int):
+    def connect_to(self, host: str, port: int):
         self.host_address = host
-        self.port_number = port
+        self.port = port
 
         connect_packet = QUIC_PACKET(FLAGS.SYN) # create a SYN type packet
-        self.sock.sendto(connect_packet.serialize_data(), (self.host_address, self.port_number))
+        self.sock.sendto(connect_packet.serialize_data(), (self.host_address, self.port))
 
         received_data, address = self.sock.recvfrom(QUIC_PACKET.Max_size)
         received_packet = QUIC_PACKET.deserialize_data(received_data)[0]
@@ -86,14 +87,19 @@ class QUIC_CONNECTION:
         print("Data sent successfully")
         self.out_streams.clear()
         final_packet = QUIC_PACKET(FLAGS.FIN)
-        self.sock.sendto(final_packet.serialize_data(), (self.host_address, self.port_number))
+        self.sock.sendto(final_packet.serialize_data(), (self.host_address, self.port))
 
     async def send_to_streams(self) -> None:
+        # generate random frame size
+        frame_size = int(random.uniform(1000, 2000))
+        # calculate the frame payload size (without the header)
+        frame_payload_size = frame_size - QUIC_PACKET.FRAME_LENGTH
+        # calculate the number of frames per packet
+        frames_per_packet = math.ceil(QUIC_PACKET.Max_size / frame_size)
+        await asyncio.gather(*(self.send_stream_data(stream_id,frame_payload_size,frames_per_packet) for stream_id in self.out_streams))
 
-        await asyncio.gather(*(self.send_stream_data(stream_id) for stream_id in self.out_streams))
 
-
-    async def send_stream_data(self, stream_id: int):
+    async def send_stream_data(self, stream_id: int, frame_payload_size: int, frames_per_packet: int) -> None:
         """
         1. get the data from each stream by stream_id
         2. generate random frame size and calculate number of frames needed.
@@ -103,30 +109,26 @@ class QUIC_CONNECTION:
         6. send the frames on the stream.
         """
         data_from_stream = self.out_streams[stream_id]
-
-        # generate random frame size
-        frame_size = int(random.uniform(1000, 2000))
-        frame_payload_size = frame_size - QUIC_PACKET.FRAME_LENGTH
         needed_frames_amount = math.ceil(len(data_from_stream) / frame_payload_size)
-        frames_per_packet = math.ceil(QUIC_PACKET.Max_size / frame_size)
         needed_packets_amount = math.ceil(needed_frames_amount / frames_per_packet)
 
         for i in range(needed_packets_amount):
+            #
             if i==0:
-                start_packet = QUIC_PACKET(FLAGS.FIRST_PACKET)
+                packet = QUIC_PACKET(FLAGS.FIRST_PACKET)
             elif i == needed_packets_amount-1:
-                end_packet = QUIC_PACKET(FLAGS.LAST_PACKET)
+                packet = QUIC_PACKET(FLAGS.LAST_PACKET)
             else:
                 packet = QUIC_PACKET(FLAGS.DATA_PACKET)
                 for frame_offset in range(frames_per_packet):
                     if frame_offset == frames_per_packet-1:
                         awaiting_data = data_from_stream[frame_offset*frame_payload_size:]
-                        packet.add_frame(stream_id,frame_offset,awaiting_data)
+                        packet.link_frame(stream_id,frame_offset,awaiting_data)
                     else:
                         awaiting_data = data_from_stream[frame_offset*frame_payload_size:(frame_offset+1)*frame_payload_size]
-                        packet.add_frame(stream_id,frame_offset,awaiting_data)
-                self.sock.sendto(packet.serialize_data(), (self.host_address, self.port_number))
-                await asyncio.sleep(0.001)
+                        packet.link_frame(stream_id,frame_offset,awaiting_data)
+            self.sock.sendto(packet.serialize_data(), (self.host_address, self.port))
+            await asyncio.sleep(0.001)
 
 
     async def receive_data(self) -> List[bytes]:
@@ -136,70 +138,97 @@ class QUIC_CONNECTION:
         3. if the stream_id is not in the streams stats dictionary,add it
         :return:
         """
+        frames_received_counter = 0
+        received_packets_amount = 0
+        received_bytes_amount = 0
+
         received_data, address = self.sock.recvfrom(QUIC_PACKET.Max_size)
         received_packet, received_frames = QUIC_PACKET.deserialize_data(received_data)
 
-        if received_packet.packet_flag != (FLAGS.SYN or FLAGS.ACK or FLAGS.SYN_ACK or FLAGS.FIN):
+        if received_packet.packet_flag != (FLAGS.SYN or
+                                           FLAGS.ACK or
+                                           FLAGS.SYN_ACK or
+                                           FLAGS.FIN):
+
+            frames_received_counter += len(received_frames)
+            # GOT THE FIRST PACKET OF THE SPECIFIC STREAM, START MEASURING TIME
             if received_packet.packet_flag == FLAGS.FIRST_PACKET:
-                self.stream_stats[received_frames[0].stream_id] = Stats(received_frames[0].stream_id,0,0,0,time.time())
+                self.streams_stats[received_frames[0].stream_id] = Stats(received_frames[0].stream_id,0,0,0,time.time())
+                if self.connection_stats[OVERALL_DATA] is None:
+                    self.connection_stats[OVERALL_DATA] = Stats(0,0,0,0,time.time())
+
+            self.streams_stats[received_frames[0].stream_id].frames_amount += len(received_frames)
+            self.connection_stats[OVERALL_DATA].frames_amount += len(received_frames)
+            # GOT THE LAST PACKET OF THE SPECIFIC STREAM, MEASURING END TIME
             if received_packet.packet_flag == FLAGS.LAST_PACKET:
-                end = time.time()
-                self.stream_stats[received_frames[0].stream_id].time = end - self.stream_stats[received_frames[0].stream_id].time
-        if received_packet.packet_flag == FLAGS.END_OF_DATA:
-            end = time.time()
-            self.stream_stats[0].time = end - self.stream_stats[0].time
-            self.print_stats()
+                self.streams_stats[received_frames[0].stream_id].time = time.time() - self.streams_stats[received_frames[0].stream_id].time
 
-        self.stream_stats[received_frames[0].stream_id].packets_amount += 1
-        self.stream_stats[received_frames[0].stream_id].bytes_amount += len(received_data)
-        self.stream_stats[0].packets_amount += 1
-        self.stream_stats[0].bytes_amount += len(received_data)
+            # GOT THE LAST PACKET OF THE PAYLOAD, MEASURING END TIME
+            if received_packet.packet_flag == FLAGS.END_OF_DATA:
+                self.connection_stats[OVERALL_DATA].time = time.time() - self.connection_stats[OVERALL_DATA].time
+                self.print_stats()
+
+            # AFTER RECEIVING PACKET, SEND ACK
+            self.sock.sendto(QUIC_PACKET(FLAGS.ACK).serialize_data(), address)
 
 
-        for frame in frames:
+        self.streams_stats[received_frames[0].stream_id].packets_amount += 1
+        self.streams_stats[received_frames[0].stream_id].total_bytes_amount += len(received_data)
+        self.connection_stats[OVERALL_DATA].packets_amount += 1
+        self.connection_stats[OVERALL_DATA].total_bytes_amount += len(received_data)
+
+
+        for frame in received_frames:
+            if frame.stream_id not in self.in_streams:
+                self.in_streams[frame.stream_id] = frame.frame_data
+            self.in_streams[frame.stream_id] += frame.frame_data
+
+
+
+        if received_packet.packet_flag == FLAGS.FIN:
+            self.terminate_connection()
+
+
 
 
     def print_stats(self):
-        for stream_id, stats in self.stream_stats.items():
+        for stream_id, stats in self.streams_stats.items():
             print(f"Stream ID: {stream_id}")
             print(f"Number of packets: {stats.packets_amount}")
             print(f"Number of frames: {stats.frames_amount}")
-            print(f"Total bytes: {stats.bytes_amount}")
+            print(f"Total bytes: {stats.total_bytes_amount}")
             print(f"Time: {stats.time}")
             print("\n")
 
+
+    def terminate_connection(self):
+        print(f"Got FIN packet, terminating connection")
+        self.sock.close()
+        self._is_closed = True
+
 class Stats:
-    def __init__(self, stream_id: int, packets_amount: int, frames_amount: int, bytes_amount: int, time: float):
+    def __init__(self, stream_id: int, packets_amount: int, frames_amount: int, bytes_amount: int, _time: float):
         self.stream_id = stream_id
         self.packets_amount = packets_amount
         self.frames_amount = frames_amount
-        self.bytes_amount = bytes_amount
-        self.time = time
-    """
-    This class represents the statistics for a stream.
-    """
-
-    def __init__(self, stream_id: int):
-        self.stream_id = stream_id
-        self.number_of_packets = 0
-        self.number_of_frames = 0
-        self.total_bytes = 0
-        self.time = time.time()
+        self.total_bytes_amount = bytes_amount
+        self.time = _time
 
 
 class QUIC_PACKET:
     packet_id_counter = 0
     Max_size = 65535
-
     HEADER_LENGTH = struct.calcsize('!BIQ')
     FRAME_LENGTH = struct.calcsize('!IIQ')
     MAX_DATA_SIZE = Max_size - HEADER_LENGTH
 
 
+
     def __init__(self,flag):
-        self.packet_ID = ++QUIC_PACKET.packet_id_counter
+        QUIC_PACKET.packet_id_counter += 1
+        self.packet_ID = QUIC_PACKET.packet_id_counter
         self.packet_flag = flag
-        self.packet_data = bytearray()
+        self.packet_data = bytearray() # using bytearray because it is mutable and can be modified
 
     @classmethod
     def deserialize_data(cls, data: bytes) -> Tuple['QUIC_PACKET', List['QUIC_FRAME']]:
@@ -225,6 +254,12 @@ class QUIC_PACKET:
     def serialize_data(self) -> bytes:
         packet_header = struct.pack('!BIQ', self.packet_flag, self.packet_ID, len(self.packet_data))
         return packet_header + self.packet_data
+
+    def link_frame(self, stream_id: int, position_in_stream: int, data: bytes) -> None:
+        frame_to_link = struct.pack('!IIQ', stream_id, position_in_stream, len(data))
+        if len(self.packet_data) + len(frame_to_link) > QUIC_PACKET.MAX_DATA_SIZE:
+            raise Exception("Frame size is too large")
+        self.packet_data += frame_to_link
 
 
 
--- sender.py
+++ sender.py
@@ -2,8 +2,11 @@ from QUIC import QUIC_CONNECTION, FLAGS
 
 def main():
     sender = QUIC_CONNECTION()
-    sender.connect('127.0.0.1', 8855)
+    sender.connect_to('127.0.0.1', 8855)
     print("Connection established with receiver")
+
+    # send data
+
 
 if __name__ == "__main__":
     main()
