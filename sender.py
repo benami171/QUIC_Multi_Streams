0ead898b37cd0f970a76e0357d24f8b93b1dcfe3
Gal Ben Ami <102553622+benami171@users.noreply.github.com>
2024-09-12 11:28:29 +0300

    removed redundant parts of code.

--- sender.py
+++ sender.py
@@ -1,26 +1,3 @@
-# from QUIC import QUIC_CONNECTION, FLAGS
-# import asyncio
-#
-# async def start_sender():
-#     quic_conn = QUIC_CONNECTION()
-#     quic_conn.connect_to('127.0.0.1', 4422)
-#     print("Connection established with receiver")
-#     await asyncio.sleep(0.001)
-#
-#     num_of_streams = int(input("Enter the desired number of streams: "))
-#
-#     with open("random_data.txt", "r") as file:
-#         file_data = file.read().encode()
-#         await quic_conn.send_data([file_data]*num_of_streams)
-#         print("Data sent")
-#         await asyncio.sleep(0.001)
-#         quic_conn.end_communication()
-#
-#
-#
-# if __name__ == "__main__":
-#     asyncio.run(start_sender())
-
 import asyncio
 from QUIC import QUIC_CONNECTION
 
